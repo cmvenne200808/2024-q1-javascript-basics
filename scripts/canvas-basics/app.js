a88267d358510c45b0865d182bef497c77fc85d2
cmvenne200808 <cmvenne200808>
2024-09-26 15:20:35 -0700

    Commiting today, haven't commited in a while. Changed a lot and add bounding shapes.

---
+++ scripts/canvas-basics/app.js
@@ -0,0 +1,38 @@
+//@ts-check
+import { SquareShape } from "./shapes/square.js";
+
+/** @type {HTMLCanvasElement} */
+//@ts-ignore canvas is an HTMLCanvasElement
+const canvas = document.getElementById("game-canvas");
+
+/** @type {CanvasRenderingContext2D} */
+//@ts-ignore ctx is an CanvasRenderingContext2D
+const ctx = canvas.getContext("2d");
+
+let s1 = new SquareShape(0, 0, ctx, canvas);
+
+let shapes = [];
+
+for (let i = 0; i < 1000; i++) {
+    shapes.push(new SquareShape(0, 0, ctx, canvas));
+}
+
+let lastTime = 0;
+
+function drawLoop(timestamp) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+	let elapsedTime = timestamp - lastTime;
+	lastTime = timestamp;
+
+    s1.draw();
+    s1.update();
+
+    for (const shape of shapes) {
+        shape.update();
+        shape.draw();
+    }
+
+	window.requestAnimationFrame(drawLoop);
+}
+
+window.requestAnimationFrame(drawLoop);
